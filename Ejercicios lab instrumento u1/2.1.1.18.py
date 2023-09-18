a71d5d0735ff363d980b7a2c14b39f0e9268992b
Gerabich <142540815+Gerabich@users.noreply.github.com>
2023-09-18 10:56:42 -0600

    Add files via upload

---
+++ Ejercicios lab instrumento u1/2.1.1.18.py
@@ -0,0 +1,11 @@
+
+
+'''
+Autor: Gerardo Antonio Garcia Vazquez
+Fecha 17 septiempre 2023
+Descripcion: Laboratorio 2.1.1.18
+'''
+
+'Ejercicio 2.1.1.18'
+print("Fundamentos","Programación","en", sep="***",end="...")
+print("Python")
---
+++ Ejercicios lab instrumento u1/2.1.1.19.py
@@ -0,0 +1,34 @@
+'''
+Autor: Gerardo Antonio Garcia Vazquez
+Fecha 17 septiempre 2023
+Descripcion: Laboratorio 2.1.1.19
+'''
+
+'Ejercicio 2.1.1.19'
+print("    *     "*2)
+print("   * *    "*2)
+print("  *   *   "*2)
+print(" *     *  "*2)
+print("***   *** "*2)
+print("  *   *   "*2)
+print("  *   *   "*2)
+print("  *****   "*2)
+
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
+
+print("    *")
+print("    *")
+print("   * *")
+print("   * *")
+print("  *   *")
+print("  *   *")
+print(" *     *")
+print(" *     *")
+print("***   ***")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *   *")
+print("  *   *")
+print("  *****")
+print("  *****")
