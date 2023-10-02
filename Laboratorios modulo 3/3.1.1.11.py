d3c7a1b850d1634ee196dd0887bb46be97b984e8
Gerabich <142540815+Gerabich@users.noreply.github.com>
2023-10-01 19:51:31 -0600

    Add files via upload

---
+++ Laboratorios modulo 3/3.1.1.11.py
@@ -0,0 +1,19 @@
+﻿'''
+Autor: Gerardo Antonio Garcia Vazquez
+Fecha: 25 septiempre 2023
+Descripcion: Laboratorio 3.1.1.11
+'''
+
+income = float(input("Introduce el ingreso anual:"))
+
+#
+# Escribe tu código aquí.
+#
+
+if income <= 85_528:
+    tax= income * 0.18 -556.02
+    
+    
+
+tax = round(tax, 0)
+print("El impuesto es:", str(tax) , "pesos")
