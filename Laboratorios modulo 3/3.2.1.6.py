af6c9a2f0300903a10f9f7bb527bab69e19832e7
Gerabich <142540815+Gerabich@users.noreply.github.com>
2023-10-01 18:28:51 -0600

    Add files via upload

---
+++ Laboratorios modulo 3/3.2.1.6.py
@@ -0,0 +1,16 @@
+﻿'''
+Autor: Gerardo Antonio Garcia Vazquez
+Fecha: 1 Octubre 2023
+Descripcion: Laboratorio 3.2.1.6
+'''
+import time
+
+# Escribe un bucle for que cuente hasta cinco.
+for i in range(1, 6):
+    # Cuerpo del bucle: imprime el número de iteración del bucle y la palabra "Mississippi".
+    print(i, "Mississippi")
+    # Cuerpo del bucle - usar: time.sleep(1)
+    time.sleep(1)
+
+# Escribe una función de impresión con el mensaje final.
+print("¡Listos o no, ahí voy!")
---
+++ Laboratorios modulo 3/3.2.1.9.py
@@ -0,0 +1,12 @@
+﻿'''
+Autor: Gerardo Antonio Garcia Vazquez
+Fecha: 1 Octubre 2023
+Descripcion: Laboratorio 3.2.1.9
+'''
+
+while True:
+    palabra = input("Ingresa una palabra: ")
+    if palabra == "chupacabra":
+        break  
+
+print("¡Has dejado el bucle con éxito")
