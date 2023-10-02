1604e3df239f59b098bec55dd0db886582b8693b
Gerabich <142540815+Gerabich@users.noreply.github.com>
2023-10-02 10:27:35 -0600

    Add files via upload

---
+++ Laboratorios modulo 3/3.2.1.14.py
@@ -0,0 +1,17 @@
+﻿'''
+Autor: Gerardo Antonio Garcia Vazquez
+Fecha: 1 Octubre 2023
+Descripcion: Laboratorio 3.2.1.14
+'''
+blocks = int(input("Ingresa el número de bloques: "))
+
+altura = 0
+bloquesu = 0  
+bloques_por_capa = 1  
+
+while bloquesu + bloques_por_capa <= blocks:
+    altura += 1 
+    bloquesu += bloques_por_capa  
+    bloques_por_capa += 1  
+
+print("La altura de la pirámide es:", altura)
